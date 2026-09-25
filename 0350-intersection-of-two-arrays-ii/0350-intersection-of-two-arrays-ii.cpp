f101113ba2e695b9cefe7cf09dcdfbf070f4218a
tanyakushwaha <kushwahatanya562@gmail.com>
2026-09-25 22:23:45 +0530

    Time: 4 ms (15.75%), Space: 15 MB (15.87%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+         vector<int> result;
+        unordered_map<int ,int> freq;
+
+        for(int x: nums1){
+           freq[x]++;
+        }
+        for(int x: nums2){
+            if(freq[x]>0){
+                result.push_back(x);
+                freq[x]--;
+            }
+        }
+        return result;
+    }
+};
